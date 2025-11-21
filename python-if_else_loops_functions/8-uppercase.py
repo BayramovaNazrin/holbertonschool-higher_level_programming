862b6ac576501726ee2c2aa170c0a20b1112e0b4
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-21 14:31:31 +0000

    ad

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,9 @@
 def uppercase(str):
     result=""
     for l in str:
-        if ord('a') <= ord(l) <= ord('z'):
+        if ord('A') <= ord(l) <= ord('Z'):
             result += l
         else: 
-            u = ord(l) + 32
+            u = ord(l) - 32
             result += chr(u)
-    return result
-print(uppercase('ABAHA'))"
+    print("{}".format(result))

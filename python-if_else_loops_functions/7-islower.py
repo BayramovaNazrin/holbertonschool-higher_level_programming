977b590c6e0809ec77b77a963c3c229ee286ee30
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-21 11:48:08 +0000

    ad

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
     return ord(c) >= ord("a") and ord(c) <= ord('z')
-print("a is {}".format("lower" if islower('a') else "upper")
+print("a => {}".format("lower" if islower('a') else "upper")

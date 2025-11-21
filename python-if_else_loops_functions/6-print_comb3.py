46311a95d86ad969c9de19f0746f5c94907f1561
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-21 10:34:16 +0000

    fw

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i != j:
+            if "{:d}{:d}".format(i, j) < "{:d}{:d}".format(j, i):
+                print("{:d}{:d}".format(i, j), end = ", "  if "{:d}{:d}".format(i, j) != str(89) else "\n")
+            else:
+                continue

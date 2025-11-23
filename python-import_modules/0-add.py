ea839568cae471a130dddf37d98d46d537f603c4
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-23 15:56:43 +0000

    sf

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0.py
+from add_0 import add
 a = 1
 b = 2
-if __name__ = "__main__":
-    print("{a} + {b} = {add(a,b)}".format(add(a,b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a+b)))

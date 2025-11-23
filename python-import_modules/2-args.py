9f417126003552eb672071bc9431691c926b284e
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-23 16:43:27 +0000

    sf

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,4 +2,7 @@
 if __name__ == "__main__":
     import sys
     print(sys.argv[1:])
-    print(len(sys.argv) - 1)
+    a = len(sys.argv)-1
+    print("{} arguments".format(a))
+    for i in range(1, a+1):
+        print("{}: {}".format(i, sys.argv[i]))

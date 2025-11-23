e065b94f73d68545fde5cca9f719aefbdfcf5856
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-23 17:00:24 +0000

    sd

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    count = len(sys.argv)-1
+    for i in range(count):
+        result += int(sys.argv[i+1])
+    print(result)

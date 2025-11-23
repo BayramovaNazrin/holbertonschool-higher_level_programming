5ac3eb4b689ef0d47339bd31139e7429a390feed
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-23 16:17:47 +0000

    sf

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -11,7 +11,7 @@ def div(a, b):
 def mul(a, b):
     return a * b
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, mul(a, b)))
+    print("{} * {} = {}".format(a, b, div(a, b)))

27dab5593538c912da0405ade2d55d882b16e67a
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-26 19:33:32 +0000

    ad

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,12 +13,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """Retrieves the width."""
+        """Retrieve the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width with validation."""
+        """Set the width with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -27,14 +27,14 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieves the height."""
+        """Retrieve the height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the height with validation."""
+        """Set the height with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = va
+        self.__height = value

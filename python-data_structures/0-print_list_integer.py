4a965021e6d0395c96202a8a679091f33fb0b47a
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-26 10:51:20 +0000

    ad

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from main-0 import print_list_integer
+    from main_0 import print_list_integer
     str = my_list
     for i in my_list:
         print("{:d}".format())

5799a38376e61877226e4ee7ba5efe5cf4ca88f4
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-28 13:04:07 +0000

    .

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+
+class BaseGeometry:
+    """class"""
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")

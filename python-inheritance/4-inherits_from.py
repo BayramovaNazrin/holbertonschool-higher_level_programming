9806756a3226f1445d8c51c8fe3678004f113acd
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-11-28 12:27:51 +0000

    ad

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """chech inherits"""
-    return isinstance(obj, a_class) and type(object) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class

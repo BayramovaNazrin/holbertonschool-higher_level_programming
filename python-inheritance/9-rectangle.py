3d37fec8a5a0a59369904008c9e5da0e8f538ab3
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-08 15:54:02 +0000

    .

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize rectangle with validated private attributes"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation"""
+        return f"[Rectangle] {self.__width}/{self.__height}"

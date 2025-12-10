0190cd4e1db3cd3f434e3eaea89f3a344f4a511f
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-10 16:17:21 +0000

    .

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 #fetch url
-
-
+"""
+Fetches https://alx-intranet.hbtn.io/status and prints the response.
+"""
 import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()

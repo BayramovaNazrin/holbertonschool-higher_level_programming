2bf468f830606c683878db57924905841e340638
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-10 18:36:36 +0000

    .

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -9,20 +9,20 @@ def fetch_and_print_posts():
     response = requests.get(API_URL)
     print(f"Status code: {response.status_code}")
     if response.status_code == 200:
-        posts = response.json
+        posts = response.json()
         for post in posts:
             print(post.get("title"))
 def fetch_and_save_posts():
     response = requests.get(API_URL)
     if response.status_code == 200:
-        posts = response.json
+        posts = response.json()
         data = [
             {
                 "id": post.get("id"),
                 "title": post.get("title"),
                 "body": post.get("body"),
             }
-            for posts in posts
+            for post in posts
         ]
         with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=["id", "title", "body"])

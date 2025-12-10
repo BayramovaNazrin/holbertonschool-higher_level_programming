0eb286415a380518d8f50950180d3df568f6f235
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-10 16:43:58 +0000

    .

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# response header value
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+"""
+import urllib.request
+import sys
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        headers = request.info()
+
+    print(headers.get("X-Request-Id"))

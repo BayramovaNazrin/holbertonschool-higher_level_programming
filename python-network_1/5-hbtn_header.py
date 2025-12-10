8f86fff0d9a31038da03ee8c55d28613ba34dc72
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-10 17:27:48 +0000

    .

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# response header value
+"""
+sends a request to the URL and displays the value of the variable X-Request-Id
+"""
+import requests
+import sys
+if __name__ == '__main__':
+    url = sys.argv[1]
+    
+    response = requests.get(url)
+    print(response.header.get("X-Request-Id"))

7adb59c10e6933a29bec17cee85188d21166d4ab
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-10 17:21:04 +0000

    .

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,14 +3,12 @@
 """
 script that fetches url
 """
-import sys
-import urllib.request
+import request
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
 
-    with urlopen.request.openurl(url) as request:
-        body = request.open()
+    response = requests.get(url)
 
-    print("Body response: {}".format(body))
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+    print("Body response:")
+    print("\t- type: {}".format(type(reponse.text)))
+    print("\t- content: {}".format(response.text))

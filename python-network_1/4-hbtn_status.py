76ed58af4d054329c7c04c5d98fb2f1561875016
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2025-12-10 17:23:05 +0000

    .

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     response = requests.get(url)
 
     print("Body response:")
-    print("\t- type: {}".format(type(reponse.text)))
+    print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
